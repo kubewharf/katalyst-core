57ad80347ed5526da08870918957a5715d631b4b
caohe <caohe9603@gmail.com>
2023-03-13 15:30:48 +0800

    fix(qrm): fix incorrect for-range closure usage in memory QRM plugin
    Signed-off-by: caohe <caohe9603@gmail.com>

--- pkg/agent/qrm-plugins/memory/dynamicpolicy/policy.go
+++ pkg/agent/qrm-plugins/memory/dynamicpolicy/policy.go
@@ -578,7 +578,7 @@ func (p *DynamicPolicy) setMemoryMigrate() {
 				}
 
 				p.migratingMemory[podUID][containerName] = true
-				go func() {
+				go func(podUID, containerName string, cgData *common.CPUSetData) {
 					defer func() {
 						p.migrateMemoryLock.Lock()
 						delete(p.migratingMemory[podUID], containerName)
@@ -605,7 +605,7 @@ func (p *DynamicPolicy) setMemoryMigrate() {
 					}
 
 					klog.Infof("[MemoryDynamicPolicy.setMemoryMigrate] set cgroup memory migrate for pod: %s, container: %s(%s) successfully", podUID, containerName, containerId)
-				}()
+				}(podUID, containerName, cgData)
 			}
 		}
 	}
