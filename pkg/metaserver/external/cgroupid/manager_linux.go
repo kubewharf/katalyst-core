be5f5ca744ed21ec21c3a2aab67546f18d341c04
luomingmeng <luomingmeng@bytedance.com>
2023-03-29 00:26:11 +0800

    fix(external manager): refine license indent

--- pkg/metaserver/external/cgroupid/manager_linux.go
+++ pkg/metaserver/external/cgroupid/manager_linux.go
@@ -1,7 +1,7 @@
 //go:build linux
 // +build linux
 
-//Copyright 2022 The Katalyst Authors.
+// Copyright 2022 The Katalyst Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
