0d257f81fb42150a932b11a4795b97fe5975b4c9
cong.xu <cong.xu@bytedance.com>
2023-08-23 14:33:19 +0800

    feat: add some uts

---
+++ pkg/util/machine/network_linux_test.go
@@ -0,0 +1,54 @@
+//go:build linux
+// +build linux
+
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package machine
+
+import (
+	"testing"
+
+	"github.com/kubewharf/katalyst-core/pkg/config/agent/global"
+	"github.com/kubewharf/katalyst-core/pkg/util/machine"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetExtraNetworkInfo(t *testing.T) {
+	t.Parallel()
+
+	conf := &global.MachineInfoConfiguration{
+		NetMultipleNS:   false,
+		NetNSDirAbsPath: "",
+	}
+
+	netInfo, err := GetExtraNetworkInfo(conf)
+	assert.Nil(t, err)
+	assert.NotNil(t, netInfo)
+}
+
+func TestGetInterfaceAttr(t *testing.T) {
+	t.Parallel()
+
+	nic := &machine.InterfaceInfo{
+		Iface: "eth0",
+	}
+
+	getInterfaceAttr(nic, "/sys/class/net")
+	assert.Nil(t, nic)
+	assert.Equal(t, -1, nic.NumaNode)
+	assert.Equal(t, false, nic.Enable)
+}
