6dbc70dc9cf8ad134e32eef6ef739561c53ea880
wangzhe.21 <wangzhe.21@bytedance.com>
2024-05-21 15:42:24 +0800

    fix: convert memory by quantity.Value

--- pkg/util/native/resources.go
+++ pkg/util/native/resources.go
@@ -283,6 +283,9 @@ func MultiplyMilliQuantity(quantity resource.Quantity, y float64) resource.Quant
 	if 0 == y {
 		return *resource.NewMilliQuantity(0, quantity.Format)
 	}
+	if 1 == y {
+		return quantity
+	}
 
 	milliValue := quantity.MilliValue()
 	if 0 == milliValue {
@@ -297,6 +300,9 @@ func MultiplyMilliQuantity(quantity resource.Quantity, y float64) resource.Quant
 func MultiplyQuantity(quantity resource.Quantity, y float64) resource.Quantity {
 	if 0 == y {
 		return *resource.NewQuantity(0, quantity.Format)
+	}
+	if 1 == y {
+		return quantity
 	}
 
 	value := quantity.Value()
