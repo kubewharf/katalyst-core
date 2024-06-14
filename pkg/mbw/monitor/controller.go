427ca2354c614a85d8dd3f853e24f75e5c003693
Hongwei Chen <hongwei.chen@bytedance.com>
2024-06-14 21:30:40 +0000

    feat(mbm): mbw lib minimum changes - license header, import correction

--- pkg/mbw/monitor/controller.go
+++ pkg/mbw/monitor/controller.go
@@ -1,11 +1,27 @@
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package monitor
 
 import (
 	"fmt"
-	msrutils "membw_manager/pkg/utils/msr"
-	"membw_manager/pkg/utils/rdt"
 	"sort"
 
+	msrutils "github.com/kubewharf/katalyst-core/pkg/mbw/utils/msr"
+	"github.com/kubewharf/katalyst-core/pkg/mbw/utils/rdt"
 	"github.com/kubewharf/katalyst-core/pkg/util/general"
 )
 
