ec2d93ff35b1c25b8460ceab02023320ab9e5173
方俊 <fangjun.1024@bytedance.com>
2024-10-23 21:38:12 +0800

    feat(sysadvisor): add offline per numa cgroup
    Change-Id: I63c0e7b7becb450bda956cd3f46310c32279a8fa

--- pkg/agent/sysadvisor/plugin/qosaware/server/cpu_server.go
+++ pkg/agent/sysadvisor/plugin/qosaware/server/cpu_server.go
@@ -271,7 +271,6 @@ func (cs *cpuServer) assembleResponse(advisorResp *types.InternalCPUCalculationR
 
 // assmble per-numa headroom
 func (cs *cpuServer) assembleHeadroom() *advisorsvc.CalculationInfo {
-	numaAllocatable, err := cs.headroomResourceManager.GetNumaAllocatable()
 	if err != nil {
 		klog.Errorf("get numa allocatable failed: %v", err)
 		return nil
