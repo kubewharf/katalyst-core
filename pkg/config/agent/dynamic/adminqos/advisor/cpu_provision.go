818988b191667a936970ac567a04ed12c3cbb2ec
linzhecheng <linzhecheng@bytedance.com>
2025-04-19 08:10:20 +0800

    refactor(*): remove useless codes
    Signed-off-by: linzhecheng <linzhecheng@bytedance.com>

--- pkg/config/agent/dynamic/adminqos/advisor/cpu_provision.go
+++ pkg/config/agent/dynamic/adminqos/advisor/cpu_provision.go
@@ -17,28 +17,14 @@ limitations under the License.
 package advisor
 
 import (
-	"k8s.io/utils/pointer"
-
 	"github.com/kubewharf/katalyst-api/pkg/apis/config/v1alpha1"
 	workloadv1alpha1 "github.com/kubewharf/katalyst-api/pkg/apis/workload/v1alpha1"
 	"github.com/kubewharf/katalyst-core/pkg/config/agent/dynamic/crd"
 )
 
-func NewRestrictConstraints() map[v1alpha1.ControlKnobName]v1alpha1.RestrictConstraints {
-	return map[v1alpha1.ControlKnobName]v1alpha1.RestrictConstraints{
-		v1alpha1.ControlKnobNonReclaimedCPURequirement: {
-			MaxUpperGap:      pointer.Float64(20),
-			MaxLowerGap:      pointer.Float64(20),
-			MaxUpperGapRatio: pointer.Float64(0.3),
-			MaxLowerGapRatio: pointer.Float64(0.3),
-		},
-	}
-}
-
 type CPUProvisionConfiguration struct {
 	AllowSharedCoresOverlapReclaimedCores bool
 	RegionIndicatorTargetConfiguration    map[v1alpha1.QoSRegionType][]v1alpha1.IndicatorTargetConfiguration
-	RestrictConstraints                   map[v1alpha1.ControlKnobName]v1alpha1.RestrictConstraints
 }
 
 func NewCPUProvisionConfiguration() *CPUProvisionConfiguration {
@@ -62,7 +48,6 @@ func NewCPUProvisionConfiguration() *CPUProvisionConfiguration {
 				},
 			},
 		},
-		RestrictConstraints: NewRestrictConstraints(),
 	}
 }
 
@@ -73,9 +58,6 @@ func (c *CPUProvisionConfiguration) ApplyConfiguration(conf *crd.DynamicConfigCR
 		if aqc.Spec.Config.AdvisorConfig.CPUAdvisorConfig.CPUProvisionConfig != nil {
 			for _, regionIndicator := range aqc.Spec.Config.AdvisorConfig.CPUAdvisorConfig.CPUProvisionConfig.RegionIndicators {
 				c.RegionIndicatorTargetConfiguration[regionIndicator.RegionType] = regionIndicator.Targets
-			}
-			for _, constraint := range aqc.Spec.Config.AdvisorConfig.CPUAdvisorConfig.CPUProvisionConfig.Constraints {
-				c.RestrictConstraints[constraint.Name] = constraint.RestrictConstraints
 			}
 		}
 		if aqc.Spec.Config.AdvisorConfig.CPUAdvisorConfig.AllowSharedCoresOverlapReclaimedCores != nil {
