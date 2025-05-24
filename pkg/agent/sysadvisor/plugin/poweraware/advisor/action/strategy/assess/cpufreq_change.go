519326d91554c54f1991fac624f71eb82fee4fe6
Hongwei Chen <hongwei.chen@bytedance.com>
2025-05-24 00:07:10 +0000

    pap: + cpu freq assessor

---
+++ pkg/agent/sysadvisor/plugin/poweraware/advisor/action/strategy/assess/cpufreq_change.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package assess
+
+import "fmt"
+
+const minMHZ = 800
+
+type cpuFreqChangeAssessor struct {
+	initFreqMhz int
+}
+
+func (c *cpuFreqChangeAssessor) Clear() {
+	c.initFreqMhz = 0
+}
+
+func (c *cpuFreqChangeAssessor) AccumulateEffect(current int) (int, error) {
+	if c.initFreqMhz < minMHZ {
+		return 0, fmt.Errorf("invalid initial frequency %d mhz", c.initFreqMhz)
+	}
+
+	if current < minMHZ {
+		return 0, fmt.Errorf("invalid current frequency %d mhz", current)
+	}
+
+	if current >= c.initFreqMhz {
+		return 0, nil
+	}
+
+	return 100 - current*100/c.initFreqMhz, nil
+}
+
+func (c *cpuFreqChangeAssessor) Update(actual int) {
+	// no need to keep track of the change, as we always compare with the initial frequency
+}
+
+func (c *cpuFreqChangeAssessor) AssessTarget(actualWatt, desiredWatt int, maxDecreasePercent int) int {
+	// keep as is if there is no room to decrease
+	if maxDecreasePercent <= 0 {
+		return actualWatt
+	}
+
+	// when there is decrease room for cpu frequency, lower the power in smaller portion to avoid the misstep
+	lowerLimit := (100 - maxDecreasePercent/2) * actualWatt / 100
+	if lowerLimit > desiredWatt {
+		return lowerLimit
+	}
+
+	return desiredWatt
+}
+
+func NewCPUFreqChangeAssessor(initMhz int) Assessor {
+	return &cpuFreqChangeAssessor{
+		initFreqMhz: initMhz,
+	}
+}
---
+++ pkg/agent/sysadvisor/plugin/poweraware/advisor/action/strategy/assess/cpufreq_change_test.go
@@ -0,0 +1,142 @@
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package assess
+
+import "testing"
+
+func Test_cpuFreqChangeAssessor_AccumulateEffect(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		initFreqMhz int
+	}
+	type args struct {
+		current int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			fields: fields{
+				initFreqMhz: 2500,
+			},
+			args: args{
+				current: 2000,
+			},
+			want:    20,
+			wantErr: false,
+		},
+		{
+			name: "negative low current freq",
+			fields: fields{
+				initFreqMhz: 2500,
+			},
+			args: args{
+				current: 777,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "higher freq no change",
+			fields: fields{
+				initFreqMhz: 2500,
+			},
+			args: args{
+				current: 2600,
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := &cpuFreqChangeAssessor{
+				initFreqMhz: tt.fields.initFreqMhz,
+			}
+			got, err := c.AccumulateEffect(tt.args.current)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AccumulateEffect() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("AccumulateEffect() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cpuFreqChangeAssessor_AssessTarget(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		initFreqMhz int
+	}
+	type args struct {
+		actualWatt         int
+		desiredWatt        int
+		maxDecreasePercent int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "happy path",
+			fields: fields{
+				initFreqMhz: 2500,
+			},
+			args: args{
+				actualWatt:         100,
+				desiredWatt:        80,
+				maxDecreasePercent: 10,
+			},
+			want: 95,
+		},
+		{
+			name: "happy path",
+			fields: fields{
+				initFreqMhz: 2500,
+			},
+			args: args{
+				actualWatt:         100,
+				desiredWatt:        80,
+				maxDecreasePercent: 0,
+			},
+			want: 100,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := &cpuFreqChangeAssessor{
+				initFreqMhz: tt.fields.initFreqMhz,
+			}
+			if got := c.AssessTarget(tt.args.actualWatt, tt.args.desiredWatt, tt.args.maxDecreasePercent); got != tt.want {
+				t.Errorf("AssessTarget() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
