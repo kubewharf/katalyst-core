619c1177bdb07e9c03d1159942bdfac373d7ad64
wangzy.17 <wangzy.17@bytedance.com>
2025-05-28 17:13:13 +0800

    feat(npd): add npd into cnc

--- go.mod
+++ go.mod
@@ -19,7 +19,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/h2non/gock v1.2.0
 	github.com/klauspost/cpuid/v2 v2.2.6
-	github.com/kubewharf/katalyst-api v0.5.4-0.20250526112725-41ffbbe244ee
+	github.com/kubewharf/katalyst-api v0.5.4-0.20250528114630-8231bbe27765
 	github.com/moby/sys/mountinfo v0.6.2
 	github.com/montanaflynn/stats v0.7.1
 	github.com/opencontainers/runc v1.1.6
---
+++ pkg/metaserver/npd/fetcher.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package npd
+
+import (
+	"context"
+
+	"github.com/kubewharf/katalyst-api/pkg/apis/node/v1alpha1"
+	"github.com/kubewharf/katalyst-core/pkg/client"
+	metaconfig "github.com/kubewharf/katalyst-core/pkg/config/agent/metaserver"
+	"github.com/kubewharf/katalyst-core/pkg/metaserver/agent/cnc"
+	"github.com/kubewharf/katalyst-core/pkg/metaserver/kcc"
+	"github.com/kubewharf/katalyst-core/pkg/util"
+)
+
+type NPDFetcher interface {
+	GetNPD(ctx context.Context) (*v1alpha1.NodeProfileDescriptor, error)
+}
+
+type DummyNPDFetcher struct {
+	NPD *v1alpha1.NodeProfileDescriptor
+}
+
+func (f *DummyNPDFetcher) GetNPD(_ context.Context) (*v1alpha1.NodeProfileDescriptor, error) {
+	return f.NPD, nil
+}
+
+type npdFetcher struct {
+	configLoader kcc.ConfigurationLoader
+}
+
+func NewNPDFetcher(clientSet *client.GenericClientSet,
+	cncFetcher cnc.CNCFetcher,
+	conf *metaconfig.KCCConfiguration,
+) NPDFetcher {
+	configLoader := kcc.NewKatalystCustomConfigLoader(clientSet, conf.ConfigCacheTTL, cncFetcher)
+	return &npdFetcher{
+		configLoader: configLoader,
+	}
+}
+
+func (f *npdFetcher) GetNPD(ctx context.Context) (*v1alpha1.NodeProfileDescriptor, error) {
+	npd := &v1alpha1.NodeProfileDescriptor{}
+	if err := f.configLoader.LoadConfig(ctx, util.NPDGVR, npd); err != nil {
+		return nil, err
+	}
+	return npd, nil
+}
