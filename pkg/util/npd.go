7bdd4a29c050f4d8e7efb367f255d1ae601fb525
wangzy.17 <wangzy.17@bytedance.com>
2025-05-28 17:13:13 +0800

    feat(npd): add npd into cnc

--- pkg/util/npd.go
+++ pkg/util/npd.go
@@ -16,7 +16,26 @@ limitations under the License.
 
 package util
 
-import "github.com/kubewharf/katalyst-api/pkg/apis/node/v1alpha1"
+import (
+	"encoding/json"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	configv1alpha1 "github.com/kubewharf/katalyst-api/pkg/apis/config/v1alpha1"
+	"github.com/kubewharf/katalyst-api/pkg/apis/node/v1alpha1"
+	"github.com/kubewharf/katalyst-core/pkg/consts"
+	"github.com/kubewharf/katalyst-core/pkg/util/general"
+)
+
+const (
+	NPDKind = "NodeProfileDescriptor"
+)
+
+var NPDGVR = metav1.GroupVersionResource(v1alpha1.SchemeGroupVersion.WithResource(v1alpha1.ResourceNameKatalystNPD))
 
 func InsertNPDScopedNodeMetrics(
 	status *v1alpha1.NodeProfileDescriptorStatus,
@@ -60,4 +79,84 @@ func InsertNPDScopedPodMetrics(
 	}
 
 	status.PodMetrics = append(status.PodMetrics, *scopedPodMetrics)
+}
+
+// KCCTargetResourceNPD is an implementation of KCCTargetResource for NPD
+type KCCTargetResourceNPD struct {
+	*unstructured.Unstructured
+}
+
+var _ KCCTargetResource = &KCCTargetResourceNPD{}
+
+func NewKCCTargetResourceNPD(obj *unstructured.Unstructured) *KCCTargetResourceNPD {
+	return &KCCTargetResourceNPD{
+		obj,
+	}
+}
+
+func (n *KCCTargetResourceNPD) GetLabelSelector() string { return "" }
+
+func (n *KCCTargetResourceNPD) GetPriority() int32 { return 0 }
+
+func (n *KCCTargetResourceNPD) GetNodeNames() []string { return nil }
+
+func (n *KCCTargetResourceNPD) GetCanary() *intstr.IntOrString { return nil }
+
+func (n *KCCTargetResourceNPD) GetPaused() bool { return false }
+
+func (n *KCCTargetResourceNPD) GetLastDuration() *time.Duration { return nil }
+
+func (n *KCCTargetResourceNPD) GetRevisionHistoryLimit() int64 { return 0 }
+
+func (n *KCCTargetResourceNPD) GetGenericStatus() configv1alpha1.GenericConfigStatus {
+	return configv1alpha1.GenericConfigStatus{}
+}
+
+func (n *KCCTargetResourceNPD) SetGenericStatus(status configv1alpha1.GenericConfigStatus) {}
+
+func (n *KCCTargetResourceNPD) GetObservedGeneration() int64 { return 0 }
+
+func (n *KCCTargetResourceNPD) SetObservedGeneration(generation int64) {}
+
+func (n *KCCTargetResourceNPD) GetCollisionCount() *int32 { return nil }
+
+func (n *KCCTargetResourceNPD) SetCollisionCount(count *int32) {}
+
+func (n *KCCTargetResourceNPD) CheckValid() bool { return true }
+
+func (n *KCCTargetResourceNPD) CheckExpired(now time.Time) bool { return false }
+
+func (n *KCCTargetResourceNPD) IsUpdated() bool { return false }
+
+func (n *KCCTargetResourceNPD) NeedValidateKCC() bool { return false }
+
+func (n *KCCTargetResourceNPD) IsPerNode() bool { return true }
+
+func (n *KCCTargetResourceNPD) GetUnstructured() *unstructured.Unstructured { return n.Unstructured }
+
+func (n *KCCTargetResourceNPD) Unmarshal(conf interface{}) error {
+	if n.Unstructured == nil || n.Object == nil {
+		return nil
+	}
+	return runtime.DefaultUnstructuredConverter.FromUnstructured(n.Object, conf)
+}
+
+func (n *KCCTargetResourceNPD) DeepCopy() KCCTargetResource {
+	return &KCCTargetResourceNPD{
+		n.Unstructured.DeepCopy(),
+	}
+}
+
+func (n *KCCTargetResourceNPD) GenerateConfigHash() (string, error) {
+	const npdConfigHashLength = 12
+	val, _, err := unstructured.NestedFieldCopy(n.Object, consts.ObjectFieldNameStatus)
+	if err != nil {
+		return "", err
+	}
+	data, err := json.Marshal(val)
+	if err != nil {
+		return "", err
+	}
+
+	return general.GenerateHash(data, npdConfigHashLength), nil
 }
