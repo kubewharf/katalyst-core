09b7790d30ffe30f91312fd076d9129f929e3202
luomingmeng <luomingmeng@bytedance.com>
2025-06-06 15:59:21 +0800

    feat(eviction): add CNR taint reporting support
    implement CNR taint reporting functionality in eviction manager to sync conditions as CNR taints
    add new reporter plugin for CNR taint updates
    enhance CNR taint utilities with merge and diff operations
    add comprehensive tests for new functionality

--- pkg/agent/evictionmanager/conditions.go
+++ pkg/agent/evictionmanager/conditions.go
@@ -19,17 +19,25 @@ package evictionmanager
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
+	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/klog/v2"
 	controllerutil "k8s.io/kubernetes/pkg/controller/util/node"
 	taintutils "k8s.io/kubernetes/pkg/util/taints"
 
+	"github.com/kubewharf/katalyst-api/pkg/apis/node/v1alpha1"
+	apiconsts "github.com/kubewharf/katalyst-api/pkg/consts"
 	pluginapi "github.com/kubewharf/katalyst-api/pkg/protocol/evictionplugin/v1alpha1"
+	reporterpluginapi "github.com/kubewharf/katalyst-api/pkg/protocol/reporterplugin/v1alpha1"
+	"github.com/kubewharf/katalyst-api/pkg/utils"
 	"github.com/kubewharf/katalyst-core/pkg/consts"
+	"github.com/kubewharf/katalyst-core/pkg/util"
 	"github.com/kubewharf/katalyst-core/pkg/util/general"
 )
 
@@ -37,6 +45,10 @@ var validNodeTaintEffects = sets.NewString(
 	string(v1.TaintEffectNoSchedule),
 	string(v1.TaintEffectPreferNoSchedule),
 	string(v1.TaintEffectNoExecute),
+)
+
+var validCNRTaintEffectQoSPrefixes = sets.NewString(
+	string(apiconsts.QoSLevelReclaimedCores),
 )
 
 func getTaintKeyFromConditionName(conditionName string) string {
@@ -87,6 +99,56 @@ func (m *EvictionManger) getNodeTaintsFromConditions() []v1.Taint {
 	return taints
 }
 
+func (m *EvictionManger) getCNRTaintsFromConditions() []v1alpha1.Taint {
+	m.conditionLock.RLock()
+	defer m.conditionLock.RUnlock()
+
+	taints := make([]v1alpha1.Taint, 0, len(m.conditions))
+
+	for conditionName, condition := range m.conditions {
+		if condition == nil {
+			continue
+		} else if condition.ConditionType != pluginapi.ConditionType_CNR_CONDITION {
+			continue
+		}
+
+		vis := make(map[v1.TaintEffect]bool)
+		taintKey := getTaintKeyFromConditionName(conditionName)
+		for _, conditionEffect := range condition.Effects {
+			qosLevel, effect, err := utils.ParseConditionEffect(conditionEffect)
+			if err != nil {
+				klog.Errorf("[eviction manager] parse condition effect %s failed with error: %v", conditionEffect, err)
+				continue
+			}
+
+			if !validCNRTaintEffectQoSPrefixes.Has(string(qosLevel)) {
+				klog.Errorf("[eviction manager] invalid cnr taint qos level: %s for condition: %s", qosLevel, conditionName)
+				continue
+			}
+
+			if !validNodeTaintEffects.Has(string(effect)) {
+				klog.Errorf("[eviction manager] invalid node taint effect: %s for condition: %s", effect, conditionName)
+				continue
+			} else if vis[effect] {
+				klog.Warningf("[eviction manager] found repeated node taint effect: %s in condition: %s", effect, conditionName)
+				continue
+			}
+
+			vis[effect] = true
+
+			taints = append(taints, v1alpha1.Taint{
+				QoSLevel: qosLevel,
+				Taint: v1.Taint{
+					Key:    taintKey,
+					Effect: effect,
+				},
+			})
+		}
+	}
+
+	return taints
+}
+
 func (m *EvictionManger) reportConditionsAsNodeTaints(ctx context.Context) {
 	var err error
 	defer func() {
@@ -119,6 +181,63 @@ func (m *EvictionManger) reportConditionsAsNodeTaints(ctx context.Context) {
 	return
 }
 
+func (m *EvictionManger) reportConditionsAsCNRTaints(ctx context.Context) {
+	var err error
+	defer func() {
+		_ = general.UpdateHealthzStateByError(reportCNRTaintHealthCheckName, err)
+	}()
+
+	cnr, err := m.metaGetter.CNRFetcher.GetCNR(ctx)
+	if err != nil {
+		klog.Errorf("[eviction manager] get cnr failed with error: %v", err)
+		return
+	}
+
+	taints := m.getCNRTaintsFromConditions()
+
+	// Get exist taints of cnr.
+	// We should not delete taints that are not managed by eviction manager.
+	currentEvictionManagerCNRTaints := util.CNRTaintSetFilter(cnr.Spec.Taints, util.IsManagedByReporterCNRTaint)
+	taintsToAdd, taintsToDel := util.CNRTaintSetDiff(taints, currentEvictionManagerCNRTaints)
+	if len(taintsToAdd) != 0 || len(taintsToDel) != 0 {
+		klog.Infof("[eviction manager] node taintsToAdd: %s\n taintsToDel: %s", cnrTaintsLog(taintsToAdd), cnrTaintsLog(taintsToDel))
+	}
+
+	// Add timeAdded to taintsToAdd.
+	for _, taintToAdd := range taintsToAdd {
+		now := metav1.Now()
+		taintToAdd.TimeAdded = &now
+	}
+
+	err = m.reportCNRTaints(ctx, taints)
+	if err != nil {
+		klog.Errorf("[eviction manager] failed to report cnr taints: %v", err)
+		return
+	}
+}
+
+func (m *EvictionManger) reportCNRTaints(ctx context.Context, taints []v1alpha1.Taint) error {
+	valueTaints, err := json.Marshal(&taints)
+	if err != nil {
+		return errors.Wrap(err, "marshal topology policy failed")
+	}
+
+	contents := []*reporterpluginapi.ReportContent{
+		{
+			GroupVersionKind: &util.CNRGroupVersionKind,
+			Field: []*reporterpluginapi.ReportField{
+				{
+					FieldType: reporterpluginapi.FieldType_Spec,
+					FieldName: util.CNRFieldNameTaints,
+					Value:     valueTaints,
+				},
+			},
+		},
+	}
+
+	return m.cnrTaintReporter.ReportContents(ctx, contents, true)
+}
+
 func nodeTaintsLog(taints []*v1.Taint) string {
 	var buf bytes.Buffer
 
@@ -129,6 +248,25 @@ func nodeTaintsLog(taints []*v1.Taint) string {
 		}
 
 		infoStr := fmt.Sprintf("key: %s, effect: %s", taint.Key, taint.Effect)
+		if i == 0 {
+			buf.WriteString(infoStr)
+		} else {
+			buf.WriteString("; " + infoStr)
+		}
+	}
+
+	return buf.String()
+}
+
+func cnrTaintsLog(taints []*v1alpha1.Taint) string {
+	var buf bytes.Buffer
+
+	for i, taint := range taints {
+		if taint == nil {
+			continue
+		}
+
+		infoStr := fmt.Sprintf("key: %s, effect: %s, qos: %s", taint.Key, taint.Effect, taint.QoSLevel)
 		if i == 0 {
 			buf.WriteString(infoStr)
 		} else {
