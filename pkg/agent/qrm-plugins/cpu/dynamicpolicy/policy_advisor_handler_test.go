5b5b89edf26f7ef39c651ae6e8f56c59028b2181
bob.lee <boblee0717@gmail.com>
2025-07-19 17:34:44 +0800

    test again

---
+++ pkg/agent/qrm-plugins/cpu/dynamicpolicy/policy_advisor_handler_test.go
@@ -0,0 +1,120 @@
+package dynamicpolicy
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/bytedance/mockey"
+	"github.com/kubewharf/katalyst-core/pkg/agent/qrm-plugins/advisorsvc"
+	advisorapi "github.com/kubewharf/katalyst-core/pkg/agent/qrm-plugins/cpu/dynamicpolicy/cpuadvisor"
+	"github.com/kubewharf/katalyst-core/pkg/metaserver"
+	"github.com/kubewharf/katalyst-core/pkg/metaserver/agent"
+	"github.com/kubewharf/katalyst-core/pkg/metaserver/agent/pod"
+	"github.com/kubewharf/katalyst-core/pkg/util/cgroup/common"
+	"github.com/kubewharf/katalyst-core/pkg/util/general"
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	resource2 "k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/kubernetes/pkg/api/v1/resource"
+)
+
+func TestDynamicPolicy_getAllDirs(t *testing.T) {
+	t.Parallel()
+
+	policy := &DynamicPolicy{}
+
+	t.Run("basic path", func(t *testing.T) {
+		t.Parallel()
+
+		_ = mockey.Mock(os.ReadDir).IncludeCurrentGoRoutine().To(func(dirname string) ([]os.DirEntry, error) {
+			return []os.DirEntry{
+				mockDirEntry{"foo", true},
+				mockDirEntry{"bar", true},
+			}, nil
+		}).Build()
+
+		dirs, err := policy.getAllDirs("/fake/path")
+		assert.NoError(t, err)
+		assert.ElementsMatch(t, dirs, []string{"foo", "bar"})
+	})
+}
+
+type mockDirEntry struct {
+	name  string
+	isDir bool
+}
+
+func (m mockDirEntry) Name() string      { return m.name }
+func (m mockDirEntry) IsDir() bool       { return m.isDir }
+func (m mockDirEntry) Type() os.FileMode { return 0 }
+func (m mockDirEntry) Info() (os.FileInfo, error) { return nil, nil }
+
+func TestDynamicPolicy_applyCgroupConfigs(t *testing.T) {
+	t.Parallel()
+
+	resources := &common.CgroupResources{
+		CpuQuota:  1000,
+		CpuPeriod: 1000,
+	}
+
+	mockBytes, _ := json.Marshal(resources)
+
+	mockResp := &advisorapi.ListAndWatchResponse{
+		ExtraEntries: []*advisorsvc.CalculationInfo{
+			{
+				CgroupPath: "test_cgroup_path",
+				CalculationResult: &advisorsvc.CalculationResult{
+					Values: map[string]string{
+						string(advisorapi.ControlKnobKeyCgroupConfig): string(mockBytes),
+					},
+				},
+			},
+		},
+	}
+
+	mockPodPathMap := map[string]*v1.Pod{
+		"test-pod-1": &v1.Pod{
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Name: "test-container",
+						Resources: v1.ResourceRequirements{
+							Requests: v1.ResourceList{
+								v1.ResourceCPU: resource2.MustParse("2"),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	cpuResourceList := v1.ResourceList{
+		v1.ResourceCPU: resource2.MustParse("2"),
+	}
+
+	p := &DynamicPolicy{
+		metaServer: &metaserver.MetaServer{
+			MetaAgent: &agent.MetaAgent{
+				PodFetcher: &pod.PodFetcherStub{},
+			},
+		},
+	}
+
+	mockey.PatchConvey("When calculating cgroup configs ", t, func() {
+		mockey.Mock(general.IsPathExists).IncludeCurrentGoRoutine().Return(true).Build()
+		mockey.Mock(common.CheckCgroup2UnifiedMode).IncludeCurrentGoRoutine().Return(false).Build()
+		mockey.Mock((*DynamicPolicy).getAllPodsPathMap).IncludeCurrentGoRoutine().Return(mockPodPathMap, nil).Build()
+		mockey.Mock(common.GetAbsCgroupPath).IncludeCurrentGoRoutine().Return("test-pod-1").Build()
+		mockey.Mock((*DynamicPolicy).getAllDirs).IncludeCurrentGoRoutine().Return([]string{"test-pod-1"}, nil).Build()
+		mockey.Mock((*DynamicPolicy).checkAllContainersQuota).IncludeCurrentGoRoutine().Return(nil).Build()
+		mockey.Mock((*DynamicPolicy).applyCPUQuotaWithRelativePath).IncludeCurrentGoRoutine().Return(nil).Build()
+		mockey.Mock(resource.PodRequestsAndLimits).IncludeCurrentGoRoutine().Return(cpuResourceList, cpuResourceList).Build()
+
+		err := p.applyCgroupConfigs(mockResp)
+
+		convey.So(err, convey.ShouldBeNil)
+	})
+} 
