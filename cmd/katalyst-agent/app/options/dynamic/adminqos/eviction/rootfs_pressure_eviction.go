15c9d992aa8078ff74e55cf27ee2691e8330aaa0
mengyipeng <mengyipeng@bytedance.com>
2025-08-20 21:07:38 +0800

    rootfs pressure eviction support system prjquota

--- cmd/katalyst-agent/app/options/dynamic/adminqos/eviction/rootfs_pressure_eviction.go
+++ cmd/katalyst-agent/app/options/dynamic/adminqos/eviction/rootfs_pressure_eviction.go
@@ -27,8 +27,10 @@ import (
 
 const (
 	defaultEnableRootfsEviction                       = false
+	defaultEnableSystemPrjquotaPressureEviction       = false
 	defaultMinimumFreeThreshold                       = ""
 	defaultMinimumInodesFreeThreshold                 = ""
+	defaultSystemPrjquotaUsedThreshold                = ""
 	defaultPodMinimumUsedThreshold                    = ""
 	defaultPodMinimumInodesUsedThreshold              = ""
 	defaultReclaimedQoSPodUsedPriorityThreshold       = ""
@@ -48,8 +50,10 @@ var (
 
 type RootfsPressureEvictionOptions struct {
 	EnableRootfsPressureEviction               bool
+	EnableSystemPrjquotaPressureEviction       bool
 	MinimumImageFsFreeThreshold                string
 	MinimumImageFsInodesFreeThreshold          string
+	SystemPrjquotaUsedThreshold                string
 	PodMinimumUsedThreshold                    string
 	PodMinimumInodesUsedThreshold              string
 	ReclaimedQoSPodUsedPriorityThreshold       string
@@ -70,8 +74,10 @@ type RootfsPressureEvictionOptions struct {
 func NewRootfsPressureEvictionOptions() *RootfsPressureEvictionOptions {
 	return &RootfsPressureEvictionOptions{
 		EnableRootfsPressureEviction:               defaultEnableRootfsEviction,
+		EnableSystemPrjquotaPressureEviction:       defaultEnableSystemPrjquotaPressureEviction,
 		MinimumImageFsFreeThreshold:                defaultMinimumFreeThreshold,
 		MinimumImageFsInodesFreeThreshold:          defaultMinimumInodesFreeThreshold,
+		SystemPrjquotaUsedThreshold:                defaultSystemPrjquotaUsedThreshold,
 		PodMinimumUsedThreshold:                    defaultPodMinimumUsedThreshold,
 		PodMinimumInodesUsedThreshold:              defaultPodMinimumInodesUsedThreshold,
 		ReclaimedQoSPodUsedPriorityThreshold:       defaultReclaimedQoSPodUsedPriorityThreshold,
@@ -91,6 +97,10 @@ func (o *RootfsPressureEvictionOptions) AddFlags(fss *cliflag.NamedFlagSets) {
 	fs := fss.FlagSet("eviction-rootfs-pressure")
 	fs.BoolVar(&o.EnableRootfsPressureEviction, "eviction-rootfs-enable", false,
 		"set true to enable rootfs pressure eviction")
+	fs.BoolVar(&o.EnableSystemPrjquotaPressureEviction, "eviction-system-prjquota-enable", false,
+		"set true to enable system prjquota pressure eviction")
+	fs.StringVar(&o.SystemPrjquotaUsedThreshold, "eviction-system-prjquota-used", "",
+		"the system prjquota used threshold for nodes. once the system used of current node is higher than this threshold, the eviction manager will try to evict some pods. example 1T, 60%")
 	fs.StringVar(&o.MinimumImageFsFreeThreshold, "eviction-rootfs-minimum-image-fs-free", "",
 		"the minimum image rootfs free threshold for nodes. once the rootfs free space of current node is lower than this threshold, the eviction manager will try to evict some pods. example 200G, 10%")
 	fs.StringVar(&o.MinimumImageFsInodesFreeThreshold, "eviction-rootfs-minimum-image-fs-inodes-free", "",
@@ -116,6 +126,7 @@ func (o *RootfsPressureEvictionOptions) AddFlags(fss *cliflag.NamedFlagSets) {
 
 func (o *RootfsPressureEvictionOptions) ApplyTo(c *eviction.RootfsPressureEvictionConfiguration) error {
 	c.EnableRootfsPressureEviction = o.EnableRootfsPressureEviction
+	c.EnableSystemPrjquotaPressureEviction = o.EnableSystemPrjquotaPressureEviction
 	if o.MinimumImageFsFreeThreshold != "" {
 		value, err := eviction.ParseThresholdValue(o.MinimumImageFsFreeThreshold)
 		if err != nil {
@@ -129,6 +140,13 @@ func (o *RootfsPressureEvictionOptions) ApplyTo(c *eviction.RootfsPressureEvicti
 			return errors.Wrapf(err, "failed to parse option: 'eviction-rootfs-minimm-inodes-free'")
 		}
 		c.MinimumImageFsInodesFreeThreshold = value
+	}
+	if o.SystemPrjquotaUsedThreshold != "" {
+		value, err := eviction.ParseThresholdValue(o.SystemPrjquotaUsedThreshold)
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse option: 'eviction-system-prjquota-used'")
+		}
+		c.SystemPrjquotaUsedThreshold = value
 	}
 	if o.PodMinimumUsedThreshold != "" {
 		value, err := eviction.ParseThresholdValue(o.PodMinimumUsedThreshold)
