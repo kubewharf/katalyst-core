07bb296dd67da728546fdcbef07b936ab0bc1f2c
jinxin.32 <jinxin.32@bytedance.com>
2025-11-04 13:42:31 +0800

    feat(borwein): add borwein v3 model support

--- pkg/config/agent/sysadvisor/qosaware/model/borwein/borwein.go
+++ pkg/config/agent/sysadvisor/qosaware/model/borwein/borwein.go
@@ -29,6 +29,7 @@ type BorweinConfiguration struct {
 	TargetIndicators                   []string
 	DryRun                             bool
 	EnableBorweinV2                    bool
+	EnableBorweinV3                    bool
 	RestrictIndicator                  bool
 }
 
@@ -48,6 +49,7 @@ func NewBorweinConfiguration() *BorweinConfiguration {
 		TargetIndicators:      []string{},
 		DryRun:                false,
 		EnableBorweinV2:       false,
+		EnableBorweinV3:       false,
 		RestrictIndicator:     false,
 	}
 }
